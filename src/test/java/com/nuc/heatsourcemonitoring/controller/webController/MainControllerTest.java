af5dcd6e4d7393f9cf36e4faee3fa5c0c99b0624
kklll <the__deep@163.com>
2020-12-19 21:05:06 +0800

    :memo: 完成提交

---
+++ src/test/java/com/nuc/heatsourcemonitoring/controller/webController/MainControllerTest.java
@@ -0,0 +1,18 @@
+package com.nuc.heatsourcemonitoring.controller.webController;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * @ClassName MainControllerTest
+ * @Deacription
+ * @Author DeepBlue
+ * @Date 2020/11/28 15:45
+ * @Version 1.0
+ **/
+class MainControllerTest {
+    @Test
+    void getRecordWithFixRate() {
+
+    }
+}
